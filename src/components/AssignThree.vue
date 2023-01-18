6ba08de9f16b73eebe2ff1ca98a864bcd3c49833
manjinder <manjinder@xyrintech.com>
2023-01-18 08:10:47 +0530

    assignment 3 with watch

--- src/components/AssignThree.vue
+++ src/components/AssignThree.vue
@@ -1,13 +1,14 @@
 <template>
   <button @click="addNum(5)">Add 5</button>
   <button @click="addNum(2)">Add 2</button>
+
   <p>{{ result }}</p>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import { computed } from "@vue/reactivity";
-
+import { watch } from "vue";
 const number = ref(0);
 const result = computed(() => {
   if (number.value < 37) {
@@ -19,7 +20,11 @@ const result = computed(() => {
     return "Too Much";
   }
 });
-
+watch(() => {
+  setTimeout(() => {
+    number.value = 0;
+  }, 5000);
+});
 const addNum = (num) => {
   return (number.value = number.value + num);
 };
